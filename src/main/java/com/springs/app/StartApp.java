b0aa4c57dcc94277182ad29cd424b4b21470e69f
Raja <rajashekary458@gmail.com>
2021-01-05 22:56:41 +0530

    test

--- src/main/java/com/springs/app/StartApp.java
+++ src/main/java/com/springs/app/StartApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartApp {
-	
+	//commit test
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
 	}
